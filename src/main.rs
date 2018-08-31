4d71ccef6fdfd4b3932e4328eb317b970af99a50
Anil N <anilknyn@fastmail.fm>
2018-08-31 23:25:35 +0530

    XFConfError

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate dbus;
+extern crate regex;
 
+// use std::collections::HashMap;
 use std::iter::FromIterator;
-use std::collections::HashMap;
+use std::error::*;
+use std::fmt;
 
 use dbus::{Connection, BusType, Message};
 use dbus::arg::{Array, Dict, Variant, RefArg};
@@ -16,6 +19,43 @@ struct XFCEDesktop {
     // monitors: Vec<String>,
 }
 
+#[derive(Debug)]
+enum XFConfError {
+    CallError(String),
+    DBusError(dbus::Error),
+}
+
+impl From<dbus::Error> for XFConfError {
+    fn from(e: dbus::Error) -> Self {
+        XFConfError::DBusError(e)
+    }
+}
+
+impl From<String> for XFConfError {
+    fn from(s: String) -> Self {
+        XFConfError::CallError(s)
+    }
+}
+
+impl Error for XFConfError {
+    fn description(&self) -> &str {
+        match self {
+            &XFConfError::CallError(ref s) => s.as_str(),
+            &XFConfError::DBusError(ref dbe) => dbe.message().unwrap_or("<NoMessage>"),
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        None
+    }
+}
+
+impl fmt::Display for XFConfError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "XFConf Error : {}", self.description())
+    }
+}
+
 impl XFCEDesktop {
     fn new() -> Result<XFCEDesktop, dbus::Error> {
         let xfcedesktop = XFCEDesktop {
@@ -24,29 +64,31 @@ impl XFCEDesktop {
         Ok(xfcedesktop)
     }
 
-    fn call_func0(&self, method: &str) -> Result<Message, dbus::Error> {
-        let m = Message::new_method_call(XFCONF_BUS, XFCONF_PATH, XFCONF_OBJ, method)
-            .map_err(|emsg| dbus::Error::new_custom("new_method_call failed", emsg.as_str()))?;
-        self.conn.send_with_reply_and_block(m, 2000)
+    #[allow(dead_code)]
+    fn call_func0(&self, method: &str) -> Result<Message, XFConfError> {
+        let m = Message::new_method_call(XFCONF_BUS, XFCONF_PATH, XFCONF_OBJ, method)?;
+        Ok(self.conn.send_with_reply_and_block(m, 2000)?)
     }
 
-    fn call_func2(&self, method: &str, p1: &str, p2: &str) -> Result<Message, dbus::Error> {
-        let m = Message::new_method_call(XFCONF_BUS, XFCONF_PATH, XFCONF_OBJ, method)
-            .map_err(|emsg| dbus::Error::new_custom("new_method_call failed", emsg.as_str()))?.append2(p1, p2);
-        self.conn.send_with_reply_and_block(m, 2000)
+    fn call_func2(&self, method: &str, p1: &str, p2: &str) -> Result<Message, XFConfError> {
+        let m = Message::new_method_call(XFCONF_BUS, XFCONF_PATH, XFCONF_OBJ, method)?.append2(p1, p2);
+        Ok(self.conn.send_with_reply_and_block(m, 2000)?)
     }
 
-    fn get_channels(&self) -> Result<Option<Vec<String>>,dbus::Error> {
+    #[allow(dead_code)]
+    fn get_channels(&self) -> Result<Option<Vec<String>>,XFConfError> {
         let m = self.call_func0("ListChannels")?;
         let arr: Array<&str,_> = m.get1().unwrap();
         Ok(Some(Vec::from_iter(arr.map(|x| x.to_owned()))))
     }
 
-    fn prop_test(&self) -> Result<(),dbus::Error> {
+    fn prop_test(&self) -> Result<(),XFConfError> {
         let m = self.call_func2("GetAllProperties", XFCONF_DESKTOP_CHANNEL, "/backdrop/screen0")?;
-        let mut z: Dict<&str, Variant<Box<RefArg>>, _> = m.get1().unwrap();
-        let mut keys = z.map(|(x,v)| x).collect::<Vec<&str>>();
+        let z: Dict<&str, Variant<Box<RefArg>>, _> = m.get1().unwrap();
+        let mut keys = z.map(|(x,_)| x).collect::<Vec<&str>>();
         keys.sort();
+
+        // let actual_monitor_re = Regex::new("/backdrop/screen0/monitorHDMI-0/workspace0/color-style").map_err(?;
         for k in keys {
             println!("{}", k);
         }
@@ -56,5 +98,8 @@ impl XFCEDesktop {
 
 fn main() {
     let xd = XFCEDesktop::new().unwrap();
-    xd.prop_test();
+    match xd.prop_test() {
+        Ok(_) => (),
+        Err(e) => println!("Boinked.. {}", e)
+    }
 }

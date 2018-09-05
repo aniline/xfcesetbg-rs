cb006d3e71419cd8cf29e657112cc33f169da6cd
Anil N <anilknyn@fastmail.fm>
2018-09-05 22:36:15 +0530

    Set multiple backdrops and more - Sorted monitor list. - Set list file. - Usage and options.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
 extern crate dbus;
 extern crate regex;
+extern crate getopts;
 
 use std::env;
-use std::collections::HashSet;
-use std::iter::FromIterator;
 use std::error::*;
 use std::fmt;
+use std::fs;
+use std::io;
 
 use dbus::{Connection, BusType, Message};
-use dbus::arg::{Array, Dict, Variant, RefArg};
+use dbus::arg::{Dict, Variant, RefArg};
 use regex::Regex;
+use getopts::Options;
 
 const XFCONF_BUS  : &str = "org.xfce.Xfconf";
 const XFCONF_PATH : &str = "/org/xfce/Xfconf";
 const XFCONF_OBJ  : &str = "org.xfce.Xfconf";
 const XFCONF_DESKTOP_CHANNEL : &str = "xfce4-desktop";
+const XFCONF_BACKDROP_LIST_PATH : &str = "/backdrop/screen0/monitor0/image-path";
 
 struct XFCEDesktop {
     conn: Connection,
-    monitors: HashSet<String>,
+    monitors: Vec<String>,
     _mon_re: Regex,
 }
 
@@ -27,6 +30,7 @@ enum XFConfError {
     CallError(String),
     DBusError(dbus::Error),
     RegexError(regex::Error),
+    IOError(io::Error),
     BadType,
     NoData,
 }
@@ -49,6 +53,12 @@ impl From<regex::Error> for XFConfError {
     }
 }
 
+impl From<io::Error> for XFConfError {
+    fn from(e: io::Error) -> Self {
+        XFConfError::IOError(e)
+    }
+}
+
 impl Error for XFConfError {
     fn description(&self) -> &str {
         match self {
@@ -57,6 +67,7 @@ impl Error for XFConfError {
             &XFConfError::RegexError(ref ree) => ree.description(),
             &XFConfError::BadType => "Incorrect type.",
             &XFConfError::NoData => "Message does not have data at that position",
+            &XFConfError::IOError(ref e) => e.description(),
         }
     }
 
@@ -78,7 +89,7 @@ impl XFCEDesktop {
     fn new() -> Result<XFCEDesktop, XFConfError> {
         let mut xfcedesktop = XFCEDesktop {
             conn: Connection::get_private(BusType::Session)?,
-            monitors: HashSet::new(),
+            monitors: Vec::new(),
             _mon_re: Regex::new(r"/backdrop/screen0/monitor(.*)/workspace0/color-style")?,
         };
 
@@ -114,14 +125,77 @@ impl XFCEDesktop {
     }
 
     /// Scrape monitors from the property list.
-    fn get_monitors(&self) -> Result<HashSet<String>,XFConfError> {
+    fn get_monitors(&self) -> Result<Vec<String>,XFConfError> {
         let m = self.call_method(self.mk_call("GetAllProperties")?.append2(XFCONF_DESKTOP_CHANNEL, "/backdrop/screen0"))?;
         let z: Dict<&str, Variant<Box<RefArg>>, _> = m.get1().unwrap();
-        let mons: HashSet<String> = HashSet::from_iter(z.map(|(x,_)| x)
-                                                       .map(|fld| self._mon_re.captures(fld))
-                                                       .filter(Option::is_some)
-                                                       .map(|c| c.unwrap().get(1).unwrap().as_str().to_string()));
-        Ok(mons)
+        let mut mons_set: Vec<String> = z.map(|(x,_)| x)
+            .map(|fld| self._mon_re.captures(fld))
+            .filter(Option::is_some)
+            .map(|c| c.unwrap().get(1).unwrap().as_str().to_string())
+            .collect();
+        mons_set.sort();
+        mons_set.dedup();
+        Ok(mons_set)
+    }
+
+    fn get_list(&self) -> Result<String, XFConfError> {
+        let m = self.call_method(self.mk_call("GetProperty")?.append2(XFCONF_DESKTOP_CHANNEL, XFCONF_BACKDROP_LIST_PATH))?;
+        let v: Variant<Box<RefArg>> = m.get1().ok_or(XFConfError::NoData)?;
+        let z: &str = v.as_str().ok_or(XFConfError::BadType)?;
+        Ok(z.to_string())
+    }
+
+    fn is_list_valid(&self, _list: &str) -> Result<(), XFConfError> {
+        let _list_attr = fs::metadata(_list)?;
+        Ok(())
+    }
+
+    fn set_list(&self, _list: &str) -> Result<(), XFConfError> {
+        self.is_list_valid(_list)?;
+
+        let list_v = Variant(_list);
+        self.call_method(self.mk_call("SetProperty")?.append3(XFCONF_DESKTOP_CHANNEL, XFCONF_BACKDROP_LIST_PATH, list_v))?;
+        Ok(())
+    }
+}
+
+fn print_usage(progname: &str, opts: Options) {
+    let brief = format!("Usage: {} [options] [IMGFILE]:[IMGFILE]:[IMGFILE].. ", progname);
+    print!("{}", opts.usage(&brief));
+}
+
+fn do_query(xfconf: &XFCEDesktop) {
+    println!("Monitors:");
+    for m in xfconf.monitors.iter() {
+        println!("{} Img = {}", m, xfconf.get_background(m, "0").unwrap());
+    }
+}
+
+fn do_setlist(xfconf: &XFCEDesktop, listfile: &String) {
+    match xfconf.get_list() {
+        Ok(list) => println!("Current list is : {}", list),
+        Err(e) =>   println!("Could not get list, {}", e),
+    }
+    println!("do_setlist(): Setting list = {}", listfile);
+    match xfconf.set_list(listfile) {
+        Err(e) => println!("Error setting list path to ({}): {}", listfile, e),
+        Ok(_) => ()
+    }
+}
+
+fn do_setimg(xfconf: &XFCEDesktop, imgfile_args: &Vec<String>) {
+    let imgfiles_collated = imgfile_args.join(":");
+    let imgfiles: Vec<&str> = imgfiles_collated.split(":").collect();
+    let imgpairs = imgfiles.iter()
+        .zip(xfconf.monitors.iter())
+        .filter(|&(i,_)| !i.is_empty());
+
+    for (i, m) in imgpairs {
+        println!("do_setimg(): Monitor: {}, Image: {}", m, i);
+        match xfconf.set_background(m, "0", i) {
+            Err(e) => println!("Failed : {} ", e),
+            Ok(_) => ()
+        }
     }
 }
 
@@ -129,15 +203,45 @@ impl XFCEDesktop {
 /// Current usage: prog <image-file>
 ///
 fn main() {
-    let ag = env::args().nth(1);
+    let args: Vec<String> = env::args().collect();
     let xfconf = XFCEDesktop::new().unwrap();
+    let progname = args[0].clone();
 
-    println!("Monitors:");
-    for m in xfconf.monitors.iter() {
-        println!("{} Img = {}", m, xfconf.get_background(m, "0").unwrap());
-        if let &Some(ref img) = &ag {
-            println!("Setting {}", &img);
-            xfconf.set_background(m, "0", img.as_str()).expect("Failed.");
+    let mut opts = Options::new();
+
+    opts.optopt  ("l", "listfile", "set backdrop list file name", "LISTFILE");
+    opts.optflag ("q", "query",    "query the current setting");
+    opts.optflag ("h", "help",     "this help");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => {
+            println!("{}", f.to_string());
+            print_usage(&progname, opts);
+            return;
+        }
+    };
+
+    if matches.opt_present("h") {
+        print_usage(&progname, opts);
+        return;
+    }
+
+    if matches.opt_present("q") {
+        do_query(&xfconf);
+        return;
+    }
+
+    if matches.opt_present("l") {
+        if let Some(listfile) = matches.opt_str("l") {
+            do_setlist(&xfconf, &listfile);
+        }
+        else {
+            print_usage(&progname, opts);
         }
     }
+
+    if !matches.free.is_empty() {
+        do_setimg(&xfconf, &matches.free);
+    };
 }
